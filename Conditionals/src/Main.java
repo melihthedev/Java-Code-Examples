559392f48d0b00256ea926947f08e495c15b0d05
Melih Cetin <melihcetin06@hotmail.com>
2022-09-20 23:52:51 +0300

    güncellendi

--- Conditionals/src/Main.java
+++ Conditionals/src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int sayi = 14;
+        int sayi = 20;
         if(sayi < 20) {
             System.out.println("Sayı 20'den küçüktür");
+        } else if (sayi == 20) {
+            System.out.println("Sayı 20'ye eşittir");
         } else {
-            System.out.println("Sayı 20'ye eşit veya 20'den büyüktür");
+            System.out.println("Sayı 20'den büyüktür");
         }
     }
 }
